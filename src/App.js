b71625d325693311454046b191669dd7910b64c4
Leon <pavlenkoleon@gmail.com>
2022-11-27 15:31:48 +0200

    designed finishes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import FrontPage from './components/FrontPage';
+import NavHeader from './components/NavHeader'
 
 function App() {
   return (
     <div className="App">
+      <NavHeader />
       <FrontPage></FrontPage>
+      <div className="footer p-2 text-white" >©Leon Pavlenko 2022</div>
     </div>
   );
 }
