13ba7bfb7dfd6509d4aca4185c4272bc96de2130
Leon <pavlenkoleon@gmail.com>
2022-11-30 17:02:44 +0200

    styled cart buttons

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -4,7 +4,23 @@ import FrontPage from '../components/HomePage/FrontPage';
 import ShopPage from './ShopPage/ShopPage';
 import ProductPage from './productPage/ProductPage';
 
+import { useState, useEffect } from 'react';
+
 function RouteSwitch() {
+  // eslint-disable-next-line no-unused-vars
+  const [counter, setCounter] = useState(0);
+  function incrementCount() {
+    setCounter((prev) => prev++);
+  }
+
+  function decrementCount() {
+    setCounter((prev) => prev--);
+  }
+
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]);
+
   return (
     <BrowserRouter>
       <NavHeader />
@@ -13,7 +29,10 @@ function RouteSwitch() {
         <Route path="/Home" element={<FrontPage />} />
         <Route path="/Contact" />
         <Route path="/Shop" element={<ShopPage />} />
-        <Route path="/products/:name" element={<ProductPage />} />
+        <Route
+          path="/products/:name"
+          element={<ProductPage handle-func={{ incrementCount, decrementCount }} />}
+        />
       </Routes>
       <div className="footer p-2 text-light">Leon Pavlenko 2022</div>
     </BrowserRouter>
--- src/components/productPage/ProductPage.js
+++ src/components/productPage/ProductPage.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 import PRODUCTS from '../data';
 
-import './productPage.css'
+import './productPage.css';
 
 function filterByName(product, name) {
   if (product.name === name) {
@@ -12,7 +14,12 @@ function filterByName(product, name) {
   return false;
 }
 
-function ProductPage() {
+function ProductPage(props) {
+  const incrementFunc = props['handle-func'].incrementCount;
+  const decrementFunc = props['handle-func'].decrementCount;
+  // const incrementFunc = handleFuncs.incrementCount;
+  // const decrementFunc = handleFuncs.decrementCount;
+
   const params = useParams();
   const [productToDom] = PRODUCTS.filter((product) => filterByName(product, params.name));
 
@@ -23,17 +30,30 @@ function ProductPage() {
           <img src={productToDom.url} alt="" />
         </div>
         <div className="product-description">
-          <h4 className='product-heading'> {productToDom.name}</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quos totam asperiores distinctio odit incidunt sit nesciunt, ex sed commodi.</p>
+          <h4 className="product-heading"> {productToDom.name}</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quos totam asperiores
+            distinctio odit incidunt sit nesciunt, ex sed commodi.
+          </p>
 
-          <p>نزیکی لە موسڵمانی کوردستان و بۆ ھەڵدانەوەی چەند لوڕستان، و خەڵکانێکی چاخی ماوەیەک ئەویش جار دەشتەکانی کوردستان باری ساڵ بەمجۆرە 
+          <p>
+            نزیکی لە موسڵمانی کوردستان و بۆ ھەڵدانەوەی چەند لوڕستان، و خەڵکانێکی چاخی ماوەیەک ئەویش
+            جار دەشتەکانی کوردستان باری ساڵ بەمجۆرە
           </p>
 
           <div className="cart-add-wrap">
-          <button id="detract-from-cart">-</button>
-          <p id="cart-amount">0</p>
-          <button id="add-to-cart">+</button>
-        </div>
+            <button id="detract-from-cart" onClick={incrementFunc()}>
+              -
+            </button>
+            <p id="cart-amount">0</p>
+            <button id="add-to-cart" onClick={console.log('uouo')}>
+              +
+            </button>
+
+            <Button id="cart-btn" variant="outline-warning" className="text-light">
+              Add to Cart
+            </Button>
+          </div>
         </div>
       </div>
     </div>
